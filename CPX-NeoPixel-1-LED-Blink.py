9ee9b75446ff8f42a73d5e734937774090bb3cbd
AngelT545 <angeltiburcio0524@gmail.com>
2026-01-26 13:59:17 -0800

    Update CPX-NeoPixel-1-LED-Blink.py

--- CPX-NeoPixel-1-LED-Blink.py
+++ CPX-NeoPixel-1-LED-Blink.py
@@ -1,12 +1,13 @@
-import board
+import neopixel
 import digitalio
 import time
+import board
 
-led = neopixel.Neopixel(board.NEOPIXEL,10)
+led = neopixel.NeoPixel(board.NEOPIXEL,10)
 led.brightness=0.1
 print(dir(led))
 while True: 
-    led[0] = (225,0,0)
+    led[0] = (225,0,100)
     time.sleep(.5)
     led[6] = (225,0,0)
     time.sleep(.5)
